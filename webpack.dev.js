af6491745fc4d236f0c554778c987364a5daf87b
thetzawaung-ton <241034270+thetzawaung-ton@users.noreply.github.com>
2026-06-14 10:31:59 +0630

    Add jest, eslint, prettier and format the code with prettier

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,9 +2,9 @@ import { merge } from "webpack-merge";
 import common from "./webpack.common.js";
 
 export default merge(common, {
-    mode: "development",
-    devtool: "inline-source-map",
-    devServer: {
-        static: "./dist",
-    }
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
 });
